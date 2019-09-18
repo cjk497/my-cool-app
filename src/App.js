c96a75e3529550d04bbb26016dcf3123b8035d0c
NYU User <nyuuser@NYUSH1465LP-MX.local>
2019-09-18 11:03:36 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         alt="Panda Bear Waving"
         src="https://www.telegraph.co.uk/content/dam/news/2016/08/23/106598324PandawaveNEWS-xlarge_trans_NvBQzQNjv4Bqeo_i_u9APj8RuoebjoAHt0k9u7HhRJvuo-ZLenGRumA.jpg"/>
       <h1>I'm a Panda Bear</h1>
-      <a href="https://www.amazon.com/Kung-Panda-Widescreen-Jack-Black/dp/B001ECQ75A">I sound like Jack Black!</a>
+      <a href="https://www.amazon.com/Kung-Panda-Widescreen-Jack-Black/dp/B001ECQ75A">Click Me! I sound like Jack Black!</a>
       <h2>我是熊猫啊 !!!</h2>
       <p></p>
 
